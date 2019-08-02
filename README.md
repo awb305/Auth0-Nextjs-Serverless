0f3d27a426fc2c3b219d6defe412729501937fa4
Andrew B <awb305@gmail.com>
2019-08-01 21:31:08 -0400

    edited README

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Auth0-Nextjs-Serverless
 
 
-1. Sign up for Auth0 & create a 'Regular Web Application'
+1. Sign up for Auth0 & create a 'Regular Web Application' (configure the callback, login urls, etc)
 2. Fill in env variables for "sample_now.json" and resave as now.json
 3. Deploy with now
--- auth/auth.js
+++ auth/auth.js
@@ -1,21 +1,24 @@
-const express = require("express");
-const session = require("express-session");
-const passport = require("passport");
-const Auth0Strategy = require("passport-auth0");
+const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
 const redis = require('redis');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 const cors = require('cors');
 
-
-require("dotenv").config();
+require('dotenv').config();
 const dev = process.env.NODE_ENV !== 'production';
 
 /* Connect to Redis */
-const client = redis.createClient(parseInt(process.env.REDIS_PORT), process.env.REDIS_HOST, {
-  no_ready_check: true
-});
+const client = redis.createClient(
+  parseInt(process.env.REDIS_PORT),
+  process.env.REDIS_HOST,
+  {
+    no_ready_check: true
+  }
+);
 client.auth(process.env.REDIS_PASSWORD, err => {
   if (err) throw err;
 });
@@ -32,9 +35,11 @@ const auth0Strategy = new Auth0Strategy(
     domain: process.env.AUTH0_DOMAIN,
     clientID: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
-    callbackURL:  dev ? 'http://localhost:3000/callback' : process.env.AUTH0_CALLBACK_URL
+    callbackURL: dev
+      ? 'http://localhost:3000/callback'
+      : process.env.AUTH0_CALLBACK_URL
   },
-  function (accessToken, refreshToken, extraParams, profile, done) {
+  function(accessToken, refreshToken, extraParams, profile, done) {
     return done(null, profile);
   }
 );
@@ -71,44 +76,50 @@ const sessionConfig = {
 };
 
 if (app.get('env') === 'production') {
-  app.set('trust proxy', 1) // trust first proxy
-  sessionConfig.cookie.secure = true // serve secure cookies
+  app.set('trust proxy', 1); // trust first proxy
+  sessionConfig.cookie.secure = true; // serve secure cookies
 }
-
 
 app.use(session(sessionConfig));
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 /* Auth Routes */
-app.get("/login", passport.authenticate("auth0", {
-  scope: "openid email profile"
-}), (req, res) => res.redirect("/"));
+app.get(
+  '/login',
+  passport.authenticate('auth0', {
+    scope: 'openid email profile'
+  }),
+  (req, res) => res.redirect('/')
+);
 
-app.get("/callback", (req, res, next) => {
-  passport.authenticate("auth0",  (err, user) => {
+app.get('/callback', (req, res, next) => {
+  passport.authenticate('auth0', (err, user) => {
     if (err) return next(err);
-    if (!user) return res.redirect("/login");
-    req.logIn(user, (err) => {
+    if (!user) return res.redirect('/login');
+    req.logIn(user, err => {
       if (err) return next(err);
-      res.redirect("/");
+      res.redirect('/');
     });
   })(req, res, next);
 });
 
-app.get("/logout", (req, res) => {
+app.get('/logout', (req, res) => {
   req.logout();
-  let BASE_URL =  dev ? 'http://localhost:3000' : process.env.ROOT_DOMAIN;
-  res.redirect(`https://${process.env.AUTH0_DOMAIN}/logout?client_id=${process.env.AUTH0_CLIENT_ID}&returnTo=${BASE_URL}`);
+  let BASE_URL = dev ? 'http://localhost:3000' : process.env.ROOT_DOMAIN;
+  res.redirect(
+    `https://${process.env.AUTH0_DOMAIN}/logout?client_id=${
+      process.env.AUTH0_CLIENT_ID
+    }&returnTo=${BASE_URL}`
+  );
 });
 
-app.get("/user", (req, res) => {
-  if(req.user){
-    res.json({user: req.user});
+app.get('/user', (req, res) => {
+  if (req.user) {
+    res.json({ user: req.user });
   } else {
-    res.json({user: undefined})
+    res.json({ user: undefined });
   }
 });
 
-module.exports = app
+module.exports = app;
